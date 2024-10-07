b530cf5c5ef71d75f21dc0ba9bd8ff2007a727de
Masoud <95829809+MasoudMsd@users.noreply.github.com>
2024-10-07 15:58:59 +0200

    Create ImportsExercise.sol

---
+++ Imports/ImportsExercise.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./SillyStringUtils.sol";
+
+contract ImportsExercise {
+    using SillyStringUtils for string;
+
+    SillyStringUtils.Haiku public haiku;
+
+    function saveHaiku(string memory _line1, string memory _line2, string memory _line3) public {
+        haiku.line1 = _line1;
+        haiku.line2 = _line2;
+        haiku.line3 = _line3;
+    }
+
+    function getHaiku() public view returns (SillyStringUtils.Haiku memory) {
+        return haiku;
+    }
+
+    function shruggieHaiku() public view returns (SillyStringUtils.Haiku memory) {
+        SillyStringUtils.Haiku memory modifiedHaiku = haiku;
+        modifiedHaiku.line3 = SillyStringUtils.shruggie(modifiedHaiku.line3);
+        return modifiedHaiku;
+    }
+}
